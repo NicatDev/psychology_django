fa9a0ff7835e6a2ffd33995be9ed35d9409f050f
NicatDev <nicat254memmedov@gmail.com>
2025-12-13 01:38:42 +0400

    main

--- authApp/admin.py
+++ authApp/admin.py
@@ -1,42 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, About, Contact, ContactInfo, Plan, Tag, Blog
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django import forms
-from .models import CustomUser
 
-class CustomUserCreationForm(forms.ModelForm):
-    """Admin paneldə user yaratmaq üçün form"""
-    class Meta:
-        model = CustomUser
-        fields = ('email', 'password', 'is_staff', 'is_active')
-
-class CustomUserChangeForm(forms.ModelForm):
-    """Admin paneldə user dəyişmək üçün form"""
-    class Meta:
-        model = CustomUser
-        fields = ('email', 'password', 'is_staff', 'is_active')
-        
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
     model = CustomUser
-    list_display = ('email', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_active')
-    search_fields = ('email',)
-    ordering = ('email',)
-
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    list_display = ('username', 'email', 'first_name', 'last_name', 'phone_number', 'is_staff', 'last_login', 'date_joined')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone_number','active_test_count')}),
     )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
-        ),
-    )
+    readonly_fields = ('last_login', 'date_joined')
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Contact)
