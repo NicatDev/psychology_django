055325ad853c8d9efdf28ac4e044488c9397a64e
NicatDev <nicat254memmedov@gmail.com>
2025-12-13 00:57:56 +0400

    main

--- core/settings.py
+++ core/settings.py
@@ -159,9 +159,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = 'static/'
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Media files (uploaded by users)
 MEDIA_URL = '/media/'
