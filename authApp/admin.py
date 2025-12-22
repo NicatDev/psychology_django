0cc0e9b8ca957b8251a8ddca668215ac8aea9a24
NicatDev <nicat254memmedov@gmail.com>
2025-12-22 21:47:44 +0400

    main

--- authApp/admin.py
+++ authApp/admin.py
@@ -4,7 +4,7 @@ from .models import CustomUser, About, Contact, ContactInfo, Plan, Tag, Blog
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('username', 'email', 'first_name', 'last_name', 'phone_number', 'is_staff', 'last_login', 'date_joined')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'phone_number', 'is_staff', 'last_login', 'date_joined','active_test_count')
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('phone_number','active_test_count')}),
     )
